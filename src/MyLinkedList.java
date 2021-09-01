ecd50ef48a6be5e3af95737f652acd7a56a41d6f
Vinay288 <vinayhiremath288@gmail.com>
2021-09-01 13:32:39 +0530

    ADDED:method to append and find key in linkedlist

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,4 +1,38 @@
 
-public class MyLinkedList {
+
+public class MyLinkedList<K>  {
+K key;
+INode head,tail;
+public MyLinkedList() {
+	this.head=null;
+	this.tail=null;
+}
+public INode find(K key) {
+	int index = 1;
+	INode currentNode = head;
+	while (currentNode != null) {
+		if (currentNode.getKey().equals(key)) {
+			return currentNode;
+		}
+		currentNode = currentNode.getNext();
+		index++;
+	}
+	return null;
+}
+public void append(INode<K> newNode) {
+	if (tail == null)
+		this.tail = newNode;
+	if (head == null)
+		this.head = newNode;
+	else
+		tail.setNext(newNode);
+	tail = newNode;
+}
+
+@Override
+public String toString() {
+	 return "MyHashMapNodes{"+head+'}';
+}
+
 
 }
