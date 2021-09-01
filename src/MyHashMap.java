8e2467226beb94efd6ecb7b61033d0e77260a5a3
Vinay288 <vinayhiremath288@gmail.com>
2021-09-01 13:31:52 +0530

    ADDED: method to get and add node in hashmap

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,4 +1,24 @@
 
-public class MyHashMap {
-
+public class MyHashMap<K,V> {
+MyLinkedList<K> myLinkedList;
+ public MyHashMap() {
+	this.myLinkedList = new MyLinkedList<>();
+}
+ public V get(K key) {
+	 Node<K,V> node = (Node<K, V>) this.myLinkedList.find(key);
+	 return (node==null)?null:node.getValue();
+ }
+ public void add(K key,V value) {
+	 Node<K, V> node = (Node<K, V>) this.myLinkedList.find(key);
+	 if(node == null) {
+		 node = new Node<>(key,value);
+		 this.myLinkedList.append(node);
+	 }
+	 else
+		 node.setValue(value);
+ }
+ @Override
+ public String toString() {
+	 return "MyHashMapNodes{"+myLinkedList+'}';
+ }
 }
