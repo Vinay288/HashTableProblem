9426cd4b97a9827e1fae26b0cdb2028215e5efc4
Vinay288 <vinayhiremath288@gmail.com>
2021-09-01 14:49:36 +0530

    ADDED:LinkedHashMap class to create LinkedList with hashcode

---
+++ src/MyLinkedHashMap.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class MyLinkedHashMap<K,V>{
+	private final int numberOfBuckets;
+	ArrayList<MyLinkedList<K>> myBuketArray;
+	public MyLinkedHashMap() {
+		this.numberOfBuckets=10;
+		this.myBuketArray=new ArrayList<MyLinkedList<K>>(numberOfBuckets);
+		for(int i=0;i<numberOfBuckets;i++) {
+			this.myBuketArray.add(null);
+		}
+	}
+	public int getBucketIndex(K key) {
+		int hashcode = Math.abs(key.hashCode());
+		int index= hashcode%10;
+//		System.out.println("key: "+key+" hashcode: "+hashcode+" index: "+index);
+		return index;
+	}
+	public V get(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBuketArray.get(index);
+		if(myLinkedList == null) return null;
+		Node<K, V> node = (Node<K, V>)myLinkedList.find(key);
+		return(node ==null)?null:node.getValue();	
+	}
+	public void add(K key,V value) {
+		int index= this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBuketArray.get(index);
+		if(myLinkedList==null) {
+			myLinkedList = new MyLinkedList<>();
+			this.myBuketArray.set(index, myLinkedList);
+		}
+		Node<K, V> node = (Node<K, V>) myLinkedList.find(key);
+		if(node ==null) {
+			node = new Node<>(key, value);
+			myLinkedList.append(node);
+		}
+		else
+			node.setValue(value);
+	}
+	@Override
+	 public String toString() {
+		 return "MyHashMapNodes{"+myBuketArray+'}';
+	 }
+
+}
